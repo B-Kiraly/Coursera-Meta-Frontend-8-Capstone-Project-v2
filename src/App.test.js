a0f4e00913835a368cf35158db2e4efc020b40f7
b-kiraly <b.m.kiraly@gmail.com>
2023-08-24 01:26:55 -0400

    Created two tests for BookingForm

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from "@testing-library/react";
+import BookingForm from "./subcomponents/BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Renders the BookingForm heading', () => {
+  const timeDispatch = () => {
+    return 1+1
+  }
+  render(<BookingForm timesDispatch={timeDispatch}/>);
+  const formLabels = ["Reservation Name", "Choose date", "Choose time", "Number of guests", "Occasion"]
+  formLabels.forEach(labelText => {
+    const labelElement = screen.getByText(labelText)
+    expect(labelElement).toBeInTheDocument()
+  })
+})
+
+test('testing to see that the expected times appear in the rendered', () => {
+  let initialTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+  const timeDispatch = () => {
+    initialTimes = [...initialTimes, "17:00"]
+  }
+  render(<BookingForm availableTimes={initialTimes} timesDispatch={timeDispatch}/>);
+  initialTimes.forEach(time => {
+    const timeText = screen.getByText(time)
+    expect(timeText).toBeInTheDocument()
+  })
+})
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -3,15 +3,26 @@ import BookingForm from "../subcomponents/BookingForm"
 
 export default function Booking() {
 
-    let initializeTimes = () => {
+    const initializeTimes = () => {
         return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
     }
 
-    let timesReducer = (state, action) => {
-        return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+    const updateTimes = () => {
+        return ["15:00", "17:00", "18:00", "20:00", "21:00"]
     }
 
-    let [availableTimes] = useReducer(timesReducer, initializeTimes())
+    function timesReducer(state, action) {
+        switch (action.type) {
+            case "initialize":
+                return initializeTimes()
+            case "update":
+                return updateTimes()
+            default:
+                return state;
+        }
+    }
+
+    let [availableTimes, timesDispatch] = useReducer(timesReducer, [])
 
     const [reservationData, setReservationData] = useState({
         name: "",
@@ -23,11 +34,12 @@ export default function Booking() {
 
     return (
         <main className="booking">
-            <h1 className="section-header">Reserve a Table</h1>
+            <h1 className="section-header" onClick={() => timesReducer("hey", "now")}>Reserve a Table</h1>
             <BookingForm
             reservationData={reservationData}
             setReservationData={setReservationData}
             availableTimes={availableTimes}
+            timesDispatch={timesDispatch}
             />
         </main>
     )
